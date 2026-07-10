18ec9d18c9a66a280f2e6efeb7525237d0001a78
mchalkle07-boop <mchalkle07@gmail.com>
2026-07-10 10:32:03 -0700

    Lesson 1 Linux notes

---
+++ Intro to Computers/Lesson 01 - Intro to Linux/lesson1_notes.py
@@ -0,0 +1,14 @@
+# **Git Commands**
+git status
+git add ./filename
+git commit -m "Name of commit"
+git clone https://repoToCloneURL
+
+.gitignore -will not commit info
+
+CTRL+SHIFT+V -nice view of file
+CTRL+Tab
+
+Windows Subset of Linux install
+cmd 
+wsl --install
