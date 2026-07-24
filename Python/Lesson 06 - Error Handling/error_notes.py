637a6d23a0bb2f08038c2e3f8a671af17644e75c
mchalkle07-boop <mchalkle07@gmail.com>
2026-07-24 07:48:04 -0700

    Lesson 04 Virtual environment added, Lesson 6 notes added

--- Python/Lesson 06 - Error Handling/error_notes.py
+++ Python/Lesson 06 - Error Handling/error_notes.py
@@ -1,7 +1,4 @@
-# import random
-
-# # randint(a, b)
-
+import random
 
 # with open("numbers.txt", "w") as f:
     
@@ -18,4 +15,30 @@
 # mn = min(numbers)
 # avg = sum(numbers) / len(numbers)
 
-# print(f"Max number: {m}\n Minimum number: {mn}\n Average of numbers: {avg}")
+# print(f"Max number: {m}\nMinimum number: {mn}\nAverage of numbers: {avg}")
+
+
+# Instructor example:
+# with open("output.txt", "w") as f:
+#     random_number = str(random.randint(1, 1000))
+#     f.writelines(random_number + "\n")
+
+# with open("output.txt", "r") as input_f:
+#     lines = input_f.readlines()
+#     lines_stripped = [int(line.strip())for line in lines]
+# print(lines_stripped)
+
+# min = 1000
+# max = 0
+# sum = 0
+# count = 0
+
+# for line in lines_stripped:
+#     if line > max:
+#         max = line
+#     if line < min:
+#         min = line
+#     count += 1
+#     sum += line
+
+# average = sum / count
